c2c879a44f0aaadc135c66c8746af2a7d6007274
prasenforu <prasenforu@hotmail.com>
2017-02-10 15:31:38 +0530

    install docker and docker storage setup

---
+++ install-docker-storage.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Install Openshift 3.3 packages
+
+ssh ose-master "yum -y install wget git net-tools bind-utils iptables-services bridge-utils pythonvirtualenv gcc bash-completion"
+
+# yum update on the master and all the nodes:
+
+for node in {ose-master,ose-hub,ose-node1}; do
+echo "Running yum update on $node" && \
+ssh $node "yum -y update"
+ssh $node "yum install -y wget git net-tools bind-utils"
+done
+
+# Install docker in all hosts.
+# Below chcon only it you OSE 3.3 on RHEL 7.3
+# OSE 3.3 on RHEL 7.3 its not required
+
+for node in {ose-master,ose-hub,ose-node1}; do
+echo "Installing Docker on $node" && \
+ssh $node "sudo yum -y install docker"
+ssh $node "sed -i \"/^OPTIONS=/ s:.*:OPTIONS=\'--selinux-enabled --insecure-registry 172.30.0.0\/16\':\" /etc/sysconfig/docker"
+scp /home/ec2-user/aws-in-openshift/docker-storage-setup $node:/etc/sysconfig/docker-storage-setup
+ssh $node "docker-storage-setup"
+ssh $node "chcon -t docker_exec_t /usr/bin/docker*"
+ssh $node "chcon -Rt svirt_sandbox_file_t /var/lib"
+ssh $node "chcon -Rt svirt_sandbox_file_t /var/db"
+ssh $node "systemctl enable docker"
+ssh $node "systemctl start docker"
+done
