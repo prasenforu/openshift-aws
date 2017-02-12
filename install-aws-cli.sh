c6084ce97feab6d50da6547fff4dad7cfd8f94c0
prasenforu <prasenforu@hotmail.com>
2017-02-12 08:29:03 +0530

    Update install-aws-cli.sh

--- install-aws-cli.sh
+++ install-aws-cli.sh
@@ -13,7 +13,7 @@ if ! type "aws" > /dev/null; then
     echo "Downloading AWS CLI package and unzip"
         wget https://s3.amazonaws.com/aws-cli/awscli-bundle.zip
         unzip awscli-bundle.zip
-
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
     echo "Execute below command with root priviledge in different terminal"
     echo ""
     echo ""
