e59a8f9d2d105e7c2d1c6d67c0887ec919eaa38b
prasenforu <prasenforu@hotmail.com>
2017-02-18 14:12:32 +0530

    OSE Installation Script

---
+++ start-ose-installation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Installing PAckages for openshift
+
+yum -y install atomic-openshift-utils
+
+# Editing ansible host file
+
+maspubip=`cat /tmp/master-pubip-$USER`
+
+sed -i 's/XXXXXXXXX/"$maspubip">/g' myconfighost
+sed -i 's/XXXXXXXXX/"$maspubip">/g' /home/ec2-user/aws-in-openshift/myconfighost
+
+# Run ansible playbook
+
+ansible-playbook -i myconfighost /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml
+
