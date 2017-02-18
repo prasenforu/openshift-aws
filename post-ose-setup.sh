f39af2f99236d2e9298a76c1cf62a5119862f8ec
prasenforu <prasenforu@hotmail.com>
2017-02-18 18:21:29 +0530

    Post OSE setup

--- post-ose-setup.sh
+++ post-ose-setup.sh
@@ -36,8 +36,8 @@ systemctl status atomic-openshift-master
 # Setting Authentication openshift
 
 yum install -y httpd-tools
-htpasswd -c /etc/origin/master/users.htpasswd admin
-htpasswd /etc/origin/master/users.htpasswd pkar
+htpasswd -c /etc/origin/master/users.htpasswd admin $admpas
+htpasswd /etc/origin/master/users.htpasswd pkar $userpas
 
 # There are some security problem with router, 
 # so need to delete router deployment then 
