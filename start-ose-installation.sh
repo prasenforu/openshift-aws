b9b22a2e6cb7b587c062d576f2aa26aa16cb8ab6
prasenforu <prasenforu@hotmail.com>
2017-03-09 15:50:58 +0530

    Update start-ose-installation.sh

--- start-ose-installation.sh
+++ start-ose-installation.sh
@@ -8,10 +8,9 @@ yum -y install atomic-openshift-utils
 
 maspubip=`cat /tmp/master-pubip-$USER`
 
-sed -i 's/XXXXXXXXX/"$maspubip">/g' myconfighost
-sed -i 's/XXXXXXXXX/"$maspubip">/g' /home/ec2-user/aws-in-openshift/myconfighost
+sed -i "s/XXXXXXXXX/$maspubip/g" myconfighost
+sed -i "s/XXXXXXXXX/$maspubip/g" /home/ec2-user/aws-in-openshift/myconfighost
 
 # Run ansible playbook
 
 ansible-playbook -i myconfighost /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml
-
