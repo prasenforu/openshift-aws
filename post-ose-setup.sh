662d823569db90dbe1549f95104a9642c33e9ddb
prasenforu <prasenforu@hotmail.com>
2017-03-09 20:16:00 +0530

    Update post-ose-setup.sh

--- post-ose-setup.sh
+++ post-ose-setup.sh
@@ -17,7 +17,7 @@ oc get nodes --show-labels
 
 oc label node ose-hub.cloud-cafe.in region="infra" zone="infranodes" --overwrite
 oc label node ose-node1.cloud-cafe.in region="primary" zone="east" --overwrite
-#oc label node ose-node2.cloud-cafe.in region="primary" zone="west" –overwrite
+oc label node ose-node2.cloud-cafe.in region="primary" zone="west" –overwrite
 
 # Check all nodes details are updated
 
@@ -36,16 +36,41 @@ systemctl status atomic-openshift-master
 # Setting Authentication openshift
 
 yum install -y httpd-tools
-htpasswd -c /etc/origin/master/users.htpasswd admin $admpas
-htpasswd /etc/origin/master/users.htpasswd pkar $userpas
+htpasswd -b -c /etc/origin/master/users.htpasswd admin $admpas
+htpasswd -b /etc/origin/master/users.htpasswd pkar $userpas
+
+# Then edit /etc/origin/master/master-config.yaml as follows
+# -------
+#identityProviders:
+# - name: my_htpasswd_provider
+# challenge: true
+# login: true
+# mappingMethod: claim
+# provider:
+# apiVersion: v1
+#   kind: HTPasswdPasswordIdentityProvider
+#   file: /etc/origin/master/users.htpasswd
+# -------
 
 # There are some security problem with router, 
 # so need to delete router deployment then 
-# Setup Router separately 
+# Setup Router separately
 
-oc delete dc/router rc/router-1 svc/router po/router-1-deploy
-oadm policy add-cluster-role-to-user cluster-admin admin
-oadm policy add-scc-to-user privileged system:serviceaccount:default:registry
-systemctl restart atomic-openshift-master
-systemctl status atomic-openshift-master
-oadm router router --replicas=1 --selector='region=infra' --credentials='/etc/origin/master/openshift-router.kubeconfig' --service-account=router --stats-password=$admpas
+# Please execute below command and In annotations section, 
+# add the this line "openshift.io/node-selector: region=infra" in the default namespace object:
+## oc edit namespace default
+# Check it updated properly or not
+## oc get namespace default -o yaml
+
+# Then execute below command
+
+#oc delete dc/router rc/router-1 svc/router po/router-1-deploy dc/docker-registry rc/docker-registry-1 rc/docker-registry-2 svc/docker-registry sa/registry sa/router
+#echo '{"kind":"ServiceAccount","apiVersion":"v1","metadata":{"name":"registry"}}' | oc create -n default -f -
+#echo '{"kind":"ServiceAccount","apiVersion":"v1","metadata":{"name":"router"}}' | oc create -n default -f -
+#oadm policy add-cluster-role-to-user cluster-admin admin
+#oadm policy add-scc-to-user privileged system:serviceaccount:default:registry
+#oadm policy add-scc-to-user privileged system:serviceaccount:default:router
+#systemctl restart atomic-openshift-master
+#systemctl status atomic-openshift-master
+#oadm registry --create --credentials=/etc/origin/master/openshift-registry.kubeconfig --service-account=registry --selector='region=infra'
+#oadm router router --replicas=1 --selector='region=infra' --credentials='/etc/origin/master/openshift-router.kubeconfig' --service-account=router --stats-password=admin2675
