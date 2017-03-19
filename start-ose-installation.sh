e1122a0d81d080ea4f63d7a43cc21b7eb02f4200
prasenforu <prasenforu@hotmail.com>
2017-03-19 05:44:24 +0530

    Update start-ose-installation.sh

--- start-ose-installation.sh
+++ start-ose-installation.sh
@@ -20,3 +20,15 @@ scp /home/ec2-user/aws-in-openshift/post-ose-setup.sh  ose-master:/root/
 scp /home/ec2-user/aws-in-openshift/reset-ip.sh  ose-master:/root/
 ssh ose-master 	"chmod 755 /root/post-ose-setup.sh"
 ssh ose-master 	"chmod 755 /root/reset-ip.sh"
+
+# Rules add for monitoring 
+
+for node in {ose-master,ose-hub,ose-node1,ose-node2}; do
+echo "Adding iptables rules on $node" && \
+ssh $node "iptables -A OS_FIREWALL_ALLOW -p tcp -m state --state NEW -m tcp --dport 9100 -j ACCEPT"
+ssh $node "systemctl restart iptables"
+done
+
+ssh ose-hub "iptables -A OS_FIREWALL_ALLOW -p tcp -m state --state NEW -m tcp --dport 9093 -j ACCEPT"
+ssh ose-hub "iptables -A OS_FIREWALL_ALLOW -p tcp -m state --state NEW -m tcp --dport 9101 -j ACCEPT"
+ssh ose-hub "systemctl restart iptables"
