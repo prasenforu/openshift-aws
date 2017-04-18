9dcc9475e605fa32d81569c4e74d0d32b79920f9
prasenforu <prasenforu@hotmail.com>
2017-04-18 19:27:14 +0530

    Update dokvgstat.sh

--- dokvgstat.sh
+++ dokvgstat.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
-export $PATH
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
 to=`vgs | grep docker-vg | awk '{print $6}' | sed 's/g/ g/;s/m/ m/;s/t/ t/'`
 fr=`vgs | grep docker-vg | awk '{print $7}' | sed 's/g/ g/;s/m/ m/;s/t/ t/'`
 echo $to | awk '{if($2 == "t") {a = ($1*1024*1024*1024); print "DOC_VG_Total " a;}}'
