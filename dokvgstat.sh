ba04eabaf8c32ac22f4378bdc6459b931058ad29
prasenforu <prasenforu@hotmail.com>
2017-04-19 14:45:46 +0530

    Update dokvgstat.sh

--- dokvgstat.sh
+++ dokvgstat.sh
@@ -5,12 +5,12 @@
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
 to=`vgs | grep docker-vg | awk '{print $6}' | sed 's/g/ g/;s/m/ m/;s/t/ t/'`
 fr=`vgs | grep docker-vg | awk '{print $7}' | sed 's/g/ g/;s/m/ m/;s/t/ t/'`
-echo $to | awk '{if($2 == "t") {a = ($1*1024*1024*1024); print "DOC_VG_Total " a;}}'
-echo $to | awk '{if($2 == "g") {a = ($1*1024*1024); print "DOC_VG_Total " a;}}'
-echo $to | awk '{if($2 == "m") {a = ($1*1024); print "DOC_VG_Total " a;}}'
-echo $fr | awk '{if($2 == "t") {b = ($1*1024*1024*1024); print "DOC_VG_Free " b;}}'
-echo $fr | awk '{if($2 == "g") {b = ($1*1024*1024); print "DOC_VG_Free " b;}}'
-echo $fr | awk '{if($2 == "m") {b = ($1*1024); print "DOC_VG_Free " b;}}'
+echo $to | awk '{if($2 == "t") {a = ($1*1000*1000*1000); print "DOC_VG_Total " a;}}'
+echo $to | awk '{if($2 == "g") {a = ($1*1000*1000); print "DOC_VG_Total " a;}}'
+echo $to | awk '{if($2 == "m") {a = ($1*1000); print "DOC_VG_Total " a;}}'
+echo $fr | awk '{if($2 == "t") {b = ($1*1000*1000*1000); print "DOC_VG_Free " b;}}'
+echo $fr | awk '{if($2 == "g") {b = ($1*1000*1000); print "DOC_VG_Free " b;}}'
+echo $fr | awk '{if($2 == "m") {b = ($1*1000); print "DOC_VG_Free " b;}}'
 
 # Using dockerinfo command
 
